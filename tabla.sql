9e7b87ed394f86aea48dd6684cb3deb3a8a8ac48
VITA <75270227+Korendovych@users.noreply.github.com>
2021-05-26 00:59:43 +0200

    Create tabla.sql

---
+++ tabla.sql
@@ -0,0 +1,12 @@
+CREATE TABLE participantes(
+evento varchar(100) PRIMARY KEY,
+nombre varchar(30),
+apellidos varchar(60),
+nacimiento varchar(20),
+email varchar(40),
+tel varchar(15)    
+);
+
+
+
+paralax
